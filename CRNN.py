b86a087e847c813f0cf8c84a4fb6c39fabc2b5f9
gsdouk <59571840+gsdoukopoulou@users.noreply.github.com>
2023-10-09 11:32:31 +0200

    recreating CRNN model

--- CRNN.py
+++ CRNN.py
@@ -1,9 +1,10 @@
+import os
 import keras
 from keras import backend as K
 
 # load model
 model = keras.models.load_model(
-    os.path.join('models' , args.model + '.h5') ,
+    os.path.join('models' , 'CRNN.h5') ,
     custom_objects={
         'class_mae': class_mae ,
         'exp': K.exp
