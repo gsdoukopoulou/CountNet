bfb39a38ab7234d5fc5b0a3037ad734a1782e8d9
gsdouk <59571840+gsdoukopoulou@users.noreply.github.com>
2023-10-09 15:09:04 +0200

    recreating CRNN model

--- CRNN.py
+++ CRNN.py
@@ -24,23 +24,27 @@ model = keras.models.load_model(
 
 model.summary()
 
-model = Sequential([
-    ZeroPadding2D(padding=(0, 0), input_shape=(1, 500, 201), name='zeropadding2d_input_1'),
-    Conv2D(64, kernel_size=(3, 3), input_shape=(1, 500, 201), activation='relu', name='conv1'),
-    Conv2D(32, kernel_size=(3, 3), activation='relu', name='conv2'),
-    MaxPooling2D(pool_size=(3, 3), name='pool1'),
-    Conv2D(128, kernel_size=(3, 3), activation='relu', name='conv3'),
-    Conv2D(64, kernel_size=(3, 3), activation='relu', name='conv4'),
-    MaxPooling2D(pool_size=(3, 3), name='pool2'),
-    Dropout(0.5, name='dropout_1'),
-    Permute((2, 1, 3), name='permute_1'),
-    Reshape((53, 1280), name='reshape_1'),
-    LSTM(40, return_sequences=True, name='lstm_1'),
-    MaxPooling1D(pool_size=2, name='maxpooling1d_1'),
-    Flatten(name='flatten_1'),
-    Dense(11, name='dense_1'),
-    Activation('softmax', name='activation_1')
-])
+model = Sequential()
+model.add(Conv2D(64, 3, 3,
+            border_mode='same',
+            input_shape=(1, 500, 201)))
+# model = Sequential([
+#     ZeroPadding2D(padding=(0, 0), input_shape=(1, 500, 201), name='zeropadding2d_input_1'),
+#     Conv2D(64, kernel_size=(3, 3), input_shape=(1, 500, 201), activation='relu', name='conv1'),
+#     Conv2D(32, kernel_size=(3, 3), activation='relu', name='conv2'),
+#     MaxPooling2D(pool_size=(3, 3), name='pool1'),
+#     Conv2D(128, kernel_size=(3, 3), activation='relu', name='conv3'),
+#     Conv2D(64, kernel_size=(3, 3), activation='relu', name='conv4'),
+#     MaxPooling2D(pool_size=(3, 3), name='pool2'),
+#     Dropout(0.5, name='dropout_1'),
+#     Permute((2, 1, 3), name='permute_1'),
+#     Reshape((53, 1280), name='reshape_1'),
+#     LSTM(40, return_sequences=True, name='lstm_1'),
+#     MaxPooling1D(pool_size=2, name='maxpooling1d_1'),
+#     Flatten(name='flatten_1'),
+#     Dense(11, name='dense_1'),
+#     Activation('softmax', name='activation_1')
+# ])
 
 # Print the model summary
 model.summary()
