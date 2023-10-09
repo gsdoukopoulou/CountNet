7ba9fb1b9f3396c9c73428d9a7645bebcd051b14
gsdouk <59571840+gsdoukopoulou@users.noreply.github.com>
2023-10-09 18:42:47 +0200

    recreating CRNN model

--- CRNN.py
+++ CRNN.py
@@ -25,9 +25,10 @@ model = keras.models.load_model(
 model.summary()
 
 model = Sequential()
-model.add(ZeroPadding2D(padding=(0, 0), dim_ordering='default', input_shape=(1, 500, 201),name='zero1'))
+model.add(ZeroPadding2D(padding=(0, 0), dim_ordering='default', input_shape=(1, 500, 201), name='zero1'))
 model.add(Conv2D(64, 3, 3, border_mode='same', activation='relu', name='conv1', dim_ordering='th'))
-model.add(Conv2D(32, 3, 3, border_mode='same', activation='relu', name='conv1', dim_ordering='th'))
+model.add(Conv2D(32, 3, 3, border_mode='same', activation='relu', name='conv2', dim_ordering='th'))
+model.add(MaxPooling2D(pool_size=(3, 3), border_mode='same', name='pool1', dim_ordering='th'))
 
 # model = Sequential([
 #     ZeroPadding2D(padding=(0, 0), input_shape=(1, 500, 201), name='zeropadding2d_input_1'),
