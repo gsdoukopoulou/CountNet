dab0962d55da35de2608ecb0349cf5040a5d867f
gsdouk <59571840+gsdoukopoulou@users.noreply.github.com>
2023-10-09 20:23:07 +0200

    recreating CRNN model

--- predict.py
+++ predict.py
@@ -6,6 +6,8 @@ import keras
 import sklearn
 import librosa
 from keras import backend as K
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, LSTM, Permute, Reshape, Dropout, MaxPooling1D, ZeroPadding2D
+from keras.models import Sequential
 
 
 eps = np.finfo(np.float64).eps # np.float64 instead of np.float
@@ -64,14 +66,35 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    # load model
-    model = keras.models.load_model(
-        os.path.join('models', args.model + '.h5'),
-        custom_objects={
-            'class_mae': class_mae,
-            'exp': K.exp
-        }
-    )
+    # # load model
+    # model = keras.models.load_model(
+    #     os.path.join('models', args.model + '.h5'),
+    #     custom_objects={
+    #         'class_mae': class_mae,
+    #         'exp': K.exp
+    #     }
+    # )
+
+    model = Sequential()
+    model.add(ZeroPadding2D(padding=(0 , 0) , dim_ordering='default' , input_shape=(1 , 500 , 201) , name='zero1'))
+    model.add(Conv2D(64 , 3 , 3 , border_mode='valid' , activation='relu' , name='conv1' , dim_ordering='th'))
+    model.add(Conv2D(32 , 3 , 3 , border_mode='valid' , activation='relu' , name='conv2' , dim_ordering='th'))
+    model.add(MaxPooling2D(pool_size=(3 , 3) , border_mode='valid' , name='pool1' , dim_ordering='th'))
+    model.add(Dropout(0.5 , name='dropout_1'))
+    model.add(Conv2D(128 , 3 , 3 , border_mode='valid' , activation='relu' , name='conv3' , dim_ordering='th'))
+    model.add(Conv2D(64 , 3 , 3 , border_mode='valid' , activation='relu' , name='conv4' , dim_ordering='th'))
+    model.add(MaxPooling2D(pool_size=(3 , 3) , border_mode='valid' , name='pool2' , dim_ordering='th'))
+    model.add(Permute((2 , 1 , 3) , name='permute_1'))
+    model.add(Reshape((53 , 1280) , name='reshape_1'))
+    model.add(LSTM(40 , return_sequences=True , name='lstm_1'))
+    model.add(MaxPooling1D(pool_length=2 , name='maxpooling1d_1'))
+    model.add(Flatten(name='flatten_1'))
+    model.add(Dense(11 , name='dense_1'))
+    model.add(Activation('softmax' , name='activation_1'))
+
+    model.compile(optimizer='adam' , loss='categorical_crossentropy' , metrics=['class_mae'])
+
+    model.load_weights('models/CRNN.h5')
 
     # print model configuration
     model.summary()
