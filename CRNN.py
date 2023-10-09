0a66cbcb37feb7cd7f862e28b3d7803a33a52d28
gsdouk <59571840+gsdoukopoulou@users.noreply.github.com>
2023-10-09 11:37:28 +0200

    recreating CRNN model

--- CRNN.py
+++ CRNN.py
@@ -2,9 +2,23 @@ import os
 import keras
 from keras import backend as K
 
+
+def class_mae(y_true, y_pred): # calculate mean absolute error
+    return K.mean(
+        K.abs(
+            K.argmax(y_pred, axis=-1) - K.argmax(y_true, axis=-1)
+        ),
+        axis=-1
+    )
+
+
 # load model
 model = keras.models.load_model(
     os.path.join('models' , 'CRNN.h5') ,
+    custom_objects={
+        'class_mae': class_mae ,
+        'exp': K.exp
+    }
 )
 
 model.summary()
