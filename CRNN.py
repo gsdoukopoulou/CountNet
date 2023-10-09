31390dacfda6dff929e832d34a5a1cf7f70a6278
gsdouk <59571840+gsdoukopoulou@users.noreply.github.com>
2023-10-09 14:14:00 +0200

    recreating CRNN model

--- CRNN.py
+++ CRNN.py
@@ -24,53 +24,23 @@ model = keras.models.load_model(
 
 model.summary()
 
-# Create a Sequential model
-model = Sequential()
-
-# Layer 1: ZeroPadding2D
-model.add(ZeroPadding2D(padding=(0, 0), input_shape=(1, 500, 201), name='zeropadding2d_input_1'))
-
-# Layer 2: Conv2D
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', name='conv1'), input_shape=(1, 500, 201))
-
-# Layer 3: Conv2D
-model.add(Conv2D(32, (3, 3), activation='relu', name='conv2'))
-
-# Layer 4: MaxPooling2D
-model.add(MaxPooling2D(pool_size=(3, 3), name='pool1'))
-
-# Layer 5: Conv2D
-model.add(Conv2D(128, (3, 3), activation='relu', name='conv3'))
-
-# Layer 6: Conv2D
-model.add(Conv2D(64, (3, 3), activation='relu', name='conv4'))
-
-# Layer 7: MaxPooling2D
-model.add(MaxPooling2D(pool_size=(3, 3), name='pool2'))
-
-# Layer 8: Dropout
-model.add(Dropout(0.5, name='dropout_1'))
-
-# Layer 9: Permute
-model.add(Permute((2, 1, 3), name='permute_1'))
-
-# Layer 10: Reshape
-model.add(Reshape((53, 1280), name='reshape_1'))
-
-# Layer 11: LSTM
-model.add(LSTM(40, return_sequences=True, name='lstm_1'))
-
-# Layer 12: MaxPooling1D
-model.add(MaxPooling1D(pool_size=2, name='maxpooling1d_1'))
-
-# Layer 13: Flatten
-model.add(Flatten(name='flatten_1'))
-
-# Layer 14: Dense
-model.add(Dense(11, name='dense_1'))
-
-# Layer 15: Activation
-model.add(Activation('softmax', name='activation_1'))
+model = Sequential([
+    ZeroPadding2D(padding=(0, 0), input_shape=(1, 500, 201), name='zeropadding2d_input_1'),
+    Conv2D(64, kernel_size=(3, 3), activation='relu', name='conv1'),
+    Conv2D(32, kernel_size=(3, 3), activation='relu', name='conv2'),
+    MaxPooling2D(pool_size=(3, 3), name='pool1'),
+    Conv2D(128, kernel_size=(3, 3), activation='relu', name='conv3'),
+    Conv2D(64, kernel_size=(3, 3), activation='relu', name='conv4'),
+    MaxPooling2D(pool_size=(3, 3), name='pool2'),
+    Dropout(0.5, name='dropout_1'),
+    Permute((2, 1, 3), name='permute_1'),
+    Reshape((53, 1280), name='reshape_1'),
+    LSTM(40, return_sequences=True, name='lstm_1'),
+    MaxPooling1D(pool_size=2, name='maxpooling1d_1'),
+    Flatten(name='flatten_1'),
+    Dense(11, name='dense_1'),
+    Activation('softmax', name='activation_1')
+])
 
 # Print the model summary
 model.summary()
