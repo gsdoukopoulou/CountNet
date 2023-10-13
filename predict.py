900592a379c5e83265eb11d406a3dec0ada8812b
gsdouk <59571840+gsdoukopoulou@users.noreply.github.com>
2023-10-13 11:36:00 +0200

    trying to fix the raw audio input

--- predict.py
+++ predict.py
@@ -26,24 +26,23 @@ def class_mae(y_true, y_pred): # calculate mean absolute error
 def count(audio, model, scaler):
     # compute STFT
     # len(audio) (80000,) (D*fs, D = 5s, fs = 16KHz)
-    X = audio
-    # X = np.abs(librosa.stft(audio, n_fft=400, hop_length=160)).T # hop length: 10ms(hop size) * fs
-    #
-    # # apply global (featurewise) standardization to mean1, var0
-    # X = scaler.transform(X)
-    #
-    # # cut to input shape length (500 frames x 201 STFT bins)
-    # X = X[:500, :]
-    #
-    # # apply l2 normalization
-    # Theta = np.linalg.norm(X, axis=1) + eps
-    # X /= np.mean(Theta)
+    X = np.abs(librosa.stft(audio, n_fft=400, hop_length=160)).T # hop length: 10ms(hop size) * fs
+
+    # apply global (featurewise) standardization to mean1, var0
+    X = scaler.transform(X)
+
+    # cut to input shape length (500 frames x 201 STFT bins)
+    X = X[:500, :]
+
+    # apply l2 normalization
+    Theta = np.linalg.norm(X, axis=1) + eps
+    X /= np.mean(Theta)
 
     # add sample dimension
     X = X[np.newaxis, ...]
 
-    # if len(model.input_shape) == 4:
-    #     X = X[:, np.newaxis, ...]
+    if len(model.input_shape) == 4:
+        X = X[:, np.newaxis, ...]
 
     print(X.shape)
     # print(model.input_shape)
@@ -80,40 +79,26 @@ if __name__ == '__main__':
 
     model = Sequential()
     # the model for the spectrogram input
-    # model.add(ZeroPadding2D(padding=(0 , 0) , dim_ordering='default' , input_shape=(1 , 500 , 201) , name='zero1'))
-    # model.add(Conv2D(64 , 3 , 3 , border_mode='valid' , activation='relu' , name='conv1' , dim_ordering='th'))
-    # model.add(Conv2D(32 , 3 , 3 , border_mode='valid' , activation='relu' , name='conv2' , dim_ordering='th'))
-    # model.add(MaxPooling2D(pool_size=(3 , 3) , border_mode='valid' , name='pool1' , dim_ordering='th'))
-    # model.add(Dropout(0.5 , name='dropout_1'))
-    # model.add(Conv2D(128 , 3 , 3 , border_mode='valid' , activation='relu' , name='conv3' , dim_ordering='th'))
-    # model.add(Conv2D(64 , 3 , 3 , border_mode='valid' , activation='relu' , name='conv4' , dim_ordering='th'))
-    # model.add(MaxPooling2D(pool_size=(3 , 3) , border_mode='valid' , name='pool2' , dim_ordering='th'))
-    # model.add(Permute((2 , 1 , 3) , name='permute_1'))
-    # model.add(Reshape((53 , 1280) , name='reshape_1'))
-    # model.add(LSTM(40 , return_sequences=True , name='lstm_1'))
-    # model.add(MaxPooling1D(pool_length=2 , name='maxpooling1d_1'))
-    # model.add(Flatten(name='flatten_1'))
-    # model.add(Dense(11 , name='dense_1'))
-    # model.add(Activation('softmax' , name='activation_1'))
-
-    # try to build the model for the raw audio input
-    model.add(ZeroPadding1D(padding=0 , input_shape=(80000 , 1) , name='zero1'))
-    model.add(Conv1D(64 , 3 , border_mode='valid' , activation='relu' , name='conv1'))
-
-    # model.add(Conv1D(32 , 3 , activation='relu' , name='conv2'))
-    # model.add(MaxPooling1D(pool_length=3 , name='pool1'))
-    # model.add(LSTM(40 , return_sequences=True , name='lstm'))
-    # model.add(MaxPooling1D(pool_length=2 , name='pool2'))
-    # num_classes = 11  # Change this based on your classification task
-    # model.add(Dense(num_classes , name='dense_1'))
-    # model.add(Activation('softmax' , name='activation_1'))
+    model.add(ZeroPadding2D(padding=(0 , 0) , dim_ordering='default' , input_shape=(1 , 500 , 201) , name='zero1'))
+    model.add(Conv2D(64 , 3 , 3 , border_mode='valid' , activation='relu' , name='conv1' , dim_ordering='th'))
+    model.add(Conv2D(32 , 3 , 3 , border_mode='valid' , activation='relu' , name='conv2' , dim_ordering='th'))
+    model.add(MaxPooling2D(pool_size=(3 , 3) , border_mode='valid' , name='pool1' , dim_ordering='th'))
+    model.add(Dropout(0.5 , name='dropout_1'))
+    model.add(Conv2D(128 , 3 , 3 , border_mode='valid' , activation='relu' , name='conv3' , dim_ordering='th'))
+    model.add(Conv2D(64 , 3 , 3 , border_mode='valid' , activation='relu' , name='conv4' , dim_ordering='th'))
+    model.add(MaxPooling2D(pool_size=(3 , 3) , border_mode='valid' , name='pool2' , dim_ordering='th'))
+    model.add(Permute((2 , 1 , 3) , name='permute_1'))
+    model.add(Reshape((53 , 1280) , name='reshape_1'))
+    model.add(LSTM(40 , return_sequences=True , name='lstm_1'))
+    model.add(MaxPooling1D(pool_length=2 , name='maxpooling1d_1'))
+    model.add(Flatten(name='flatten_1'))
+    model.add(Dense(11 , name='dense_1'))
+    model.add(Activation('softmax' , name='activation_1'))
 
     model.summary()
     model.compile(optimizer='adam' , loss='categorical_crossentropy' , metrics=[class_mae])
 
-    model.load_weights('models/CRNN.h5', by_name=True) #, skip_mismatch = True
-
-    # print model configuration
+    model.load_weights('models/CRNN.h5') #, skip_mismatch = True
 
     # save as svg file
     # load standardisation parameters
