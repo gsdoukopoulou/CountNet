44c2151f75804c2445765706649f32dcb00aa917
gsdouk <59571840+gsdoukopoulou@users.noreply.github.com>
2023-10-18 21:04:14 +0200

    encoder

--- predict.py
+++ predict.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
 
     model = Sequential()
     # encoder
-    model.add(MaxPooling1D(pool_length=2, stride=None, border_mode='valid'))
+    model.add(MaxPooling1D(pool_length=2, stride=None, input_shape=(1 , 80000),  border_mode='valid'))
     model.add(Conv1D(64, 3, border_mode='same',name='encoder')) # input_shape=(1 , 80000),
     # model.add(Reshape((1 , 500 , 64) , name='reshape_layer'))
     # # the model for the spectrogram input
