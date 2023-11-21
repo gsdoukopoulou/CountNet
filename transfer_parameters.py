7d5ed02d51954c46512730b6559040daa5e2022a
gsdouk <59571840+gsdoukopoulou@users.noreply.github.com>
2023-11-21 14:42:46 +0100

    adding countnet++

--- transfer_parameters.py
+++ transfer_parameters.py
@@ -2,12 +2,13 @@ import tensorflow as tf
 import keras
 from keras import backend as K
 from keras.models import load_model, Sequential
-
+import numpy as np
 
 def class_mae(y_true, y_pred): # calculate mean absolute error
     return K.mean(K.abs(K.argmax(y_pred, axis=-1) - K.argmax(y_true, axis=-1)),axis=-1)
 
 model = load_model('/home/gsdoukopoul/CountNet/models/CRNN.h5', custom_objects={'class_mae': class_mae})
-weights = model.get_weights()
+# weights = model.get_weights()
 
-
+weights = [layer.get_weights() for layer in model.layers]
+np.savez('/home/gsdoukopoul/CountNet/models/weights.npz', *weights)
